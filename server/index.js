a8a2d51337284d47e6c914d0ab8d3c2ab0779902
choeqq <k.chola@op.pl>
2022-06-09 20:06:47 +0200

    initial server sertup

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/graphql", graphqlHTTP({}));
+
+app.listen(port, console.log(`server running on port ${port}`));
