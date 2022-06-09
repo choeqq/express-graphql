7c029e7a01084ddd35a85b1c8bf47fa2f518038e
choeqq <k.chola@op.pl>
2022-06-09 20:25:19 +0200

    clients field in schema added

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,6 +4,7 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 
 const ClientType = new GraphQLObjectType({
@@ -19,6 +20,12 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: () => ({
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(paren, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
